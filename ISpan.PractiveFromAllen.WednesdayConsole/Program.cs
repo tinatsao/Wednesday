b0c60a2f446c33c5ae4fe355ff60bdb8aa376530
tinatsao <tina192837456@gmail.com>
2023-01-07 22:21:05 +0800

    加入專案檔案。

---
+++ ISpan.PractiveFromAllen.WednesdayConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using Day;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISpan.PractiveFromAllen.WednesdayConsole
+{
+	internal class Program
+	{/// <summary>
+	/// 回傳2023年中，每一個星期三。
+	/// </summary>
+	/// <param name="args"></param>
+		static void Main(string[] args)
+		{
+			foreach(var item in Wednesday.FindWeekInYear(2023,DayOfWeek.Wednesday))
+			{
+				Console.WriteLine(item.ToString("yyyy/MM/dd"));
+			}
+		}
+	}
+}
---
+++ IsWednesday/Day.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day
+{
+    public class Wednesday
+    {//先找出第一天星期三是誰
+     //再來+7天，找到所有星期三
+     //結束
+        public static List<DateTime> FindWeekInYear(int year, DayOfWeek week)
+        {
+            var dayStart = new DateTime(year, 01,01); //今年第一天
+			var dayEnd = new DateTime(year, 12, 31);//今年最後一天
+            List<DateTime> weekInYear = new List<DateTime>(); //存找到的日期回傳
+
+            //找到第一個指定的星期
+			do
+            {
+                dayStart = dayStart.AddDays(1);
+            } while (dayStart.DayOfWeek != week);
+
+            //找到每一個指定的星期
+			for (DateTime dt = dayStart; dt < dayEnd; dt = dt.AddDays(7))
+			{
+				weekInYear.Add(dt);
+			};
+
+			return weekInYear;
+        }
+    }
+}
